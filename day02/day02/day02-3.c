75543c570671ae1e1e9d0ffdf0ece43abe142e63
202402077 <donghwan2525@gmail.com>
2024-09-12 17:28:22 +0900

    day 02

---
+++ day02/day02/day02-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b;
+	for (a = 0; a <= 99; a++) {
+		if ((a % 3 == 0) && (a % 4 == 0)) {
+			printf("%d\n", a);
+		}
+		else if(a % 7 == 0) {
+			printf("%d\n", a);
+		}
+		else {
+
+		}
+
+		
+
+		
+	}
+	return 0;
+}
